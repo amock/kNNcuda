af5b6dca285eca456098c80df3f6bfb37f3a144a
aock <amock@uos.de>
2016-11-23 20:25:29 +0100

    first version bruteforce NN search on random 3D points

--- src/kNN.cu
+++ src/kNN.cu
@@ -4,48 +4,61 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
+#include <stdexcept>
+
+#define POINT_SIZE 10000
+#define K_NEAR 20
 
 struct point {
     float x;
     float y;
-//    float z;
+    float z;
+    float operator[](int index) {
+        switch(index){
+			case 0: return x;
+			case 1: return y;
+			case 2: return z;
+			default: throw std::invalid_argument("Index out of bounds."); 
+		}
+    }
 };
 
 struct pairSort {
     bool operator()(std::pair<int,float> &left, std::pair<int,float> &right){
-        return left.second > right.second;
+        return left.second < right.second;
     }
 };
 
 float euclideanDistance(point p1, point p2) {
-    return (p1.x - p2.x)*(p1.x-p2.x) + (p1.y - p2.y)*(p1.y - p2.y);
+	float dist = 0.0;
+	for(int i = 0; i < 3; i++){
+		dist += (p1[i] - p2[i])*(p1[i]-p2[i]);
+	}
+    return dist;
 }
 
 std::vector<std::pair<int,float> > kNearestNeighbor(int index, std::vector<point> p, int k=1 ) {
     point p1 = p[index];
     std::vector<std::pair<int,float> > bestIndexDist;
-    bestIndexDist.resize(k);
+    bestIndexDist.resize(p.size()-1);
 
     for(int i=0; i<k; i++) {
         bestIndexDist[i].first = -1;
         bestIndexDist[i].second = FLT_MAX;
     }
-
+	int realindex = 0;
     for(int i=0; i<p.size(); i++) {
         if(i != index) {
             float currentDist = euclideanDistance(p1, p[i]);
-
-            for(int j=0; j<k; j++) {
-                if(currentDist < bestIndexDist[j].second) {
-                    bestIndexDist[j].first = i;
-                    bestIndexDist[j].second = currentDist;
-                    break;
-                }
-            }
-            std::sort(bestIndexDist.begin(),bestIndexDist.end(), pairSort());
+			bestIndexDist[realindex].first = i;
+			bestIndexDist[realindex].second = currentDist;
+            realindex++;
         }
     }
-    return bestIndexDist;
+    
+    std::sort(bestIndexDist.begin(),bestIndexDist.end(), pairSort());
+    
+    return std::vector<std::pair<int,float> > (bestIndexDist.begin(),bestIndexDist.begin()+k);
 }
 
 int nearestNeighbor(int index, point* p, int length) {
@@ -67,26 +80,37 @@ int nearestNeighbor(int index, point* p, int length) {
 }
 
 int main(int argc, char** argv) {
-
+	clock_t prgstart, prgend;
+	
+	int seed = time(NULL);
+	std::cout << "seed: " << seed << std::endl;
+	std::cout << "Start program with values:  " << std::endl;
+	std::cout << "  POINT_SIZE = " << POINT_SIZE << std::endl;
+	std::cout << "  K          = " << K_NEAR << std::endl; 
+	srand(seed);
+	prgstart = clock();
+	
     std::vector<point> randoms;
-    randoms.resize(100);
-    std::cout << randoms.size() << std::endl;
-    int k = 20;
-    std::vector<std::pair<int, float> > NNs;
-
-
-    srand(time(NULL));
-    for(int i=0; i<100; i++) {
-
+    randoms.resize(POINT_SIZE);
+    
+    
+    for(int i=0; i<randoms.size(); i++) {
         randoms[i].x = (float)rand() / RAND_MAX * 100.0;
         randoms[i].y = (float)rand() / RAND_MAX * 100.0;
-//        randoms[i].z = (float)rand() / RAND_MAX * 100.0;
+        randoms[i].z = (float)rand() / RAND_MAX * 100.0;
     }
-    std::cout << randoms.size() << std::endl;
-    NNs = kNearestNeighbor(0, randoms, k);
-    std::cout << NNs.size() << std::endl;
-    for(int i= 0; i<k;i++){
-        printf("Nearest Neighbor of (%f, %f): (%f, %f)\n", randoms[0].x, randoms[0].y, randoms[NNs[i].first].x, randoms[NNs[i].first].y);
-    }
+    
+    
+    for(int i=0; i<randoms.size(); i++){
+		std::vector<std::pair<int, float> > NNs = kNearestNeighbor(i, randoms, K_NEAR);
+	}
+	
+    //for(int i= 0; i<NNs.size();i++){
+    //    printf("%d Nearest Neighbor of (%f, %f, %f): (%f, %f, %f) , %f\n",i+1 ,randoms[0][0], randoms[0][1], randoms[0][2], randoms[NNs[i].first][0], randoms[NNs[i].first][1], randoms[NNs[i].first][2], NNs[i].second);
+    //}
+    
+    prgend=clock();
+	printf("Laufzeit insgesamt %f seconds\n",(float)(prgend-prgstart) / CLOCKS_PER_SEC);
+	
     return 0;
 }
