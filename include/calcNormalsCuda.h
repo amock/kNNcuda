9da0b68ad8836255bea1b9471dbda9ad73497452
aock <amock@uos.de>
2017-02-14 11:24:10 +0100

    added class including example

---
+++ include/calcNormalsCuda.h
@@ -0,0 +1,156 @@
+/**
+ * calcNormalsCuda.h
+ *
+ * @author Alexander Mock
+ * @author Matthias Greshake
+ */
+
+#ifndef __CALCNORMALSCUDA_H
+#define __CALCNORMALSCUDA_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <vector>
+#include <math.h>
+#include <float.h>
+#include <stdint.h>
+#include "boost/shared_array.hpp"
+
+typedef boost::shared_array<unsigned int> uintArr;
+
+
+typedef boost::shared_array<float> floatArr;
+
+
+typedef boost::shared_array<unsigned char> ucharArr;
+
+
+struct PointArray {
+    int width;
+    int dim;
+    float* elements;
+};
+
+//~ Device Functions
+//~ __global__ void KNNKernel(const PointArray D_V, const PointArray D_kd_tree, PointArray D_Result_Normals, int k=50);
+
+
+class CalcNormalsCuda {
+
+public:
+	
+	/**
+	 * @brief Constructor.
+	 *
+     * @param points Input Pointcloud for kd-tree construction
+	 */
+	CalcNormalsCuda(PointArray& points);
+	
+	~CalcNormalsCuda();
+	
+	/**
+	 * @brief Starts calculation the normals on GPU
+	 *
+	 */
+	void start();
+	
+	/**
+	 * @brief Get the resulting normals of the normal calculation. After calling "start".
+	 * 
+	 * @param output_normals 	PointArray as return value
+	 */
+	void getNormals(PointArray& output_normals);
+	
+	/**
+	 * @brief Set the number of k nearest neighbors
+	 *        k-neighborhood
+	 *
+	 * @param k             The size of the used k-neighborhood
+	 *
+	 */
+	void setK(int k);
+	
+	/**
+	 * @brief Set the viewpoint to orientate the normals
+	 *
+	 * @param v_x 	Coordinate X axis
+	 * @param v_y 	Coordinate Y axis
+	 * @param v_z 	Coordinate Z axis
+	 *
+	 */
+	void setFlippoint(float v_x, float v_y, float v_z);
+	
+	/**
+	 * @brief Set Method for normal calculation
+	 *
+	 * @param method   "PCA","RANSAC"
+	 *
+	 */
+	void setMethod(std::string method);
+	
+	
+private:
+	//~ Hostfunctions
+	void init();
+	
+	void printSettings();
+	
+	void getCudaInformation();
+	
+	void mallocPointArray(PointArray& m);
+
+	void generateHostPointArray(PointArray& m, int width, int dim);
+
+	void generateDevicePointArray(PointArray& D_m, int width, int dim);
+
+	void copyToDevicePointArray(PointArray& m, PointArray& D_m);
+
+	void copyToHostPointArray(PointArray& D_m, PointArray& m);
+	
+	void fillPointArrayWithSequence(PointArray& m);
+
+	void copyDimensionToPointArray(PointArray& in, int dim, PointArray& out);
+	
+	void copyVectorInterval(PointArray& in, int start, int end, PointArray& out);
+	
+	void naturalMergeSort(PointArray& in, int dim, PointArray& indices,  PointArray& m, int limit=-1);
+	
+	void mergeHostWithIndices(float* a, float* b, int i1, int j1, int i2, int j2, int limit=-1);
+	
+	void sortByDim(PointArray& V, int dim, PointArray& indices, PointArray& values);
+	
+	void splitPointArray(PointArray& I, PointArray& I_L, PointArray& I_R);
+	
+	void splitPointArrayWithValue(PointArray& V, PointArray& I, PointArray& I_L, PointArray& I_R, int current_dim, float value);
+	
+	void generateKdTreeRecursive(PointArray& V, PointArray* sorted_indices, int current_dim, int max_dim, PointArray& kd_tree, int size, int max_tree_depth, int position);
+	
+	void generateKdTreeArray(PointArray& V, PointArray* sorted_indices, int max_dim, PointArray& kd_tree);
+	
+	// Divice Function
+	void GPU_NN(PointArray& D_V, PointArray& D_kd_tree, PointArray& D_Result_Normals, PointArray& Result_Normals);
+	
+	void initKdTree();
+	
+	
+	
+	// V->points and normals
+	PointArray V, kd_tree, Result_Normals;
+	float m_vx, m_vy, m_vz;
+	int m_k;
+	
+	
+	int m_calc_method;
+
+	// Device Information
+	int m_mps;
+	int m_threads_per_mp;
+	int m_threads_per_block;
+	int* m_size_thread_block;
+	int* m_size_grid;
+	unsigned long long m_device_global_memory;
+		
+};
+
+#endif // !__CALCNORMALSCUDA_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,65 +1,520 @@
-/**
- * main.cpp
- *
- * @author Alexander Mock
- * @author Matthias Greshake
- */
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include "../include/calcNormalsCuda.h"
+#include "../include/rply/rply.h"
+#include "../include/helper_cuda.h"
 
-#include "../include/calcNormals.h"
 
-int m_mps, m_cuda_cores_per_mp, m_threads_per_mp, m_threads_per_block;
-int* m_size_block = new int(3);
-int* m_size_grid = new int(3);
-unsigned long long m_device_global_memory;
+int readVertexCb( p_ply_argument argument )
+{
+    float ** ptr;
+    ply_get_argument_user_data( argument, (void **) &ptr, NULL );
+    **ptr = ply_get_argument_value( argument );
+    (*ptr)++;
+    return 1;
 
-void getDeviceInformation(int &mps, int &cuda_cores_per_mp, int &threads_per_mp, int &threads_per_block, int* size_block, int* size_grid, unsigned long long &device_global_memory) {
-	cudaDeviceProp deviceProp;
-
-	cudaSetDevice(0);
-	cudaGetDeviceProperties(&deviceProp, 0);
-
-	mps = deviceProp.multiProcessorCount;
-	cuda_cores_per_mp = _ConvertSMVer2Cores(deviceProp.major, deviceProp.minor);
-	threads_per_mp = deviceProp.maxThreadsPerMultiProcessor;
-	threads_per_block = deviceProp.maxThreadsPerBlock;
-	size_block[0] = deviceProp.maxThreadsDim[0];
-	size_block[1] = deviceProp.maxThreadsDim[1];
-	size_block[2] = deviceProp.maxThreadsDim[2];
-	size_grid[0] = deviceProp.maxGridSize[0];
-	size_grid[1] = deviceProp.maxGridSize[1];
-	size_grid[2] = deviceProp.maxGridSize[2];
-	device_global_memory = (unsigned long long)deviceProp.totalGlobalMem;
 }
 
-int main(int argc, char** argv) {
-        const char* file;
-        const char* dest = "normals.ply";
+int readColorCb( p_ply_argument argument )
+{
 
-	getDeviceInformation(m_mps, m_cuda_cores_per_mp, m_threads_per_mp, m_threads_per_block, m_size_block, m_size_grid, m_device_global_memory);
+    uint8_t ** color;
+    ply_get_argument_user_data( argument, (void **) &color, NULL );
+    **color = ply_get_argument_value( argument );
+    (*color)++;
+    return 1;
 
-	for (int i = 0; i < argc; i++) {
-		if (strcmp(argv[i], "--info") == 0) {
-			cout << "Device Information:" << endl;
-			cout << "Multi Processors: " << m_mps << endl;
-			cout << "CUDA Cores per Multi Processor: " << m_cuda_cores_per_mp << endl;
-			cout << "Threads per Multi Processor: " << m_threads_per_mp << endl;
-			cout << "Threads per Block: " << m_threads_per_block << endl;
-			cout << "Thread Dimension: " << m_size_block[0] << ", " << m_size_block[1] << ", " << m_size_block[2] << endl;
-			cout << "Grid Size: " << m_size_grid[0] << ", " << m_size_grid[1] << ", " << m_size_grid[2] << endl;
-			cout << "Device Global Memory: " << m_device_global_memory << endl;
-			cout << endl;
-		}
-                if (strcmp(argv[i], "--file") == 0) {
-                        file = argv[i+1];
+}
+
+int readFaceCb( p_ply_argument argument )
+{
+
+    unsigned int ** face;
+    long int length, value_index;
+    ply_get_argument_user_data( argument, (void **) &face, NULL );
+    ply_get_argument_property( argument, NULL, &length, &value_index );
+    if ( value_index < 0 )
+    {
+        /* We got info about amount of face vertices. */
+        if ( ply_get_argument_value( argument ) == 3 )
+        {
+            return 1;
+        }
+        std::cerr  << "Mesh is not a triangle mesh." << std::endl;
+        return 0;
+    }
+    **face = ply_get_argument_value( argument );
+    (*face)++;
+
+    return 1;
+
+}
+
+void readPlyFile(PointArray& V, size_t& num_points, const char* filename ,float scale=1.0, int number_of_data = -1,bool readColor =true, bool readConfidence=true,
+    bool readIntensity=true, bool readNormals=true, bool readFaces=true){
+	/* Start reading new PLY */
+    p_ply ply = ply_open( filename, NULL, 0, NULL );
+
+    if ( !ply )
+    {
+       std::cerr  << "Could not open »" << filename << "«."
+           << std::endl;
+        return ;
+    }
+    if ( !ply_read_header( ply ) )
+    {
+       std::cerr  << "Could not read header." << std::endl;
+        return;
+    }
+    std::cout  << "Loading »" << filename << "«." << std::endl;
+
+    /* Check if there are vertices and get the amount of vertices. */
+    char buf[256] = "";
+    const char * name = buf;
+    long int n;
+    p_ply_element elem  = NULL;
+
+    // Buffer count variables
+    size_t numVertices              = 0;
+    size_t numVertexColors          = 0;
+    size_t numVertexConfidences     = 0;
+    size_t numVertexIntensities     = 0;
+    size_t numVertexNormals         = 0;
+
+    size_t numPoints                = 0;
+    size_t numPointColors           = 0;
+    size_t numPointConfidence       = 0;
+    size_t numPointIntensities      = 0;
+    size_t numPointNormals          = 0;
+    size_t numFaces                 = 0;
+
+    while ( ( elem = ply_get_next_element( ply, elem ) ) )
+    {
+        ply_get_element_info( elem, &name, &n );
+        if ( !strcmp( name, "vertex" ) )
+        {
+            numVertices = n;
+            p_ply_property prop = NULL;
+            while ( ( prop = ply_get_next_property( elem, prop ) ) )
+            {
+                ply_get_property_info( prop, &name, NULL, NULL, NULL );
+                if ( !strcmp( name, "red" ) && readColor )
+                {
+                    /* We have color information */
+                    numVertexColors = n;
                 }
+                else if ( !strcmp( name, "confidence" ) && readConfidence )
+                {
+                    /* We have confidence information */
+                    numVertexConfidences = n;
+                }
+                else if ( !strcmp( name, "intensity" ) && readIntensity )
+                {
+                    /* We have intensity information */
+                    numVertexIntensities = n;
+                }
+                else if ( !strcmp( name, "nx" ) && readNormals )
+                {
+                    /* We have normals */
+                    numVertexNormals = n;
+                }
+            }
+        }
+        else if ( !strcmp( name, "point" ) )
+        {
+            numPoints = n;
+            p_ply_property prop = NULL;
+            while ( ( prop = ply_get_next_property( elem, prop ) ) )
+            {
+                ply_get_property_info( prop, &name, NULL, NULL, NULL );
+                if ( !strcmp( name, "red" ) && readColor )
+                {
+                    /* We have color information */
+                    numPointColors = n;
+                }
+                else if ( !strcmp( name, "confidence" ) && readConfidence )
+                {
+                    /* We have confidence information */
+                    numPointConfidence = n;
+                }
+                else if ( !strcmp( name, "intensity" ) && readIntensity )
+                {
+                    /* We have intensity information */
+                    numPointIntensities = n;
+                }
+                else if ( !strcmp( name, "nx" ) && readNormals )
+                {
+                    /* We have normals */
+                    numPointNormals = n;
+                }
+            }
+        }
+        else if ( !strcmp( name, "face" ) && readFaces )
+        {
+            numFaces = n;
+        }
+    }
+
+    if ( !( numVertices || numPoints ) )
+    {
+        std::cout << "Neither vertices nor points in ply."
+            << std::endl;
+        return ;
+    }
+
+    // Buffers
+    floatArr vertices;
+    floatArr vertexConfidence;
+    floatArr vertexIntensity;
+    floatArr vertexNormals;
+    floatArr points;
+    floatArr pointConfidences;
+    floatArr pointIntensities;
+    floatArr pointNormals;
+
+    ucharArr pointColors;
+	ucharArr vertexColors;
+    uintArr  faceIndices;
+
+
+    /* Allocate memory. */
+    if ( numVertices )
+    {
+        vertices = floatArr( new float[ numVertices * 3 ] );
+    }
+    if ( numVertexColors )
+    {
+        vertexColors = ucharArr( new unsigned char[ numVertices * 3 ] );
+    }
+    if ( numVertexConfidences )
+    {
+        vertexConfidence = floatArr( new float[ numVertices ] );
+    }
+    if ( numVertexIntensities )
+    {
+        vertexIntensity = floatArr( new float[ numVertices ] );
+    }
+    if ( numVertexNormals )
+    {
+        vertexNormals = floatArr( new float[ 3 * numVertices ] );
+    }
+    if ( numFaces )
+    {
+        faceIndices = uintArr( new unsigned int[ numFaces * 3 ] );
+    }
+    if ( numPoints )
+    {
+        points = floatArr( new float[ numPoints * 3 ] );
+    }
+    if ( numPointColors )
+    {
+        pointColors = ucharArr( new unsigned char[ numPoints * 3 ] );
+    }
+    if ( numPointConfidence )
+    {
+        pointConfidences = floatArr( new float[numPoints] );
+    }
+    if ( numPointIntensities )
+    {
+        pointIntensities = floatArr( new float[numPoints] );
+    }
+    if ( numPointNormals )
+    {
+        pointNormals = floatArr( new float[ 3 * numPoints ] );
+    }
+
+
+    float*        vertex            = vertices.get();
+    uint8_t* 	  vertex_color      = vertexColors.get();
+    float*        vertex_confidence = vertexConfidence.get();
+    float*        vertex_intensity  = vertexIntensity.get();
+    float*        vertex_normal     = vertexNormals.get();
+    unsigned int* face              = faceIndices.get();
+    float*        point             = points.get();
+    uint8_t*      point_color       = pointColors.get();
+    float*        point_confidence  = pointConfidences.get();
+    float*        point_intensity   = pointIntensities.get();
+    float*        point_normal      = pointNormals.get();
+
+
+    /* Set callbacks. */
+    if ( vertex )
+    {
+        ply_set_read_cb( ply, "vertex", "x", readVertexCb, &vertex, 0 );
+        ply_set_read_cb( ply, "vertex", "y", readVertexCb, &vertex, 0 );
+        ply_set_read_cb( ply, "vertex", "z", readVertexCb, &vertex, 1 );
+    }
+    if ( vertex_color )
+    {
+        ply_set_read_cb( ply, "vertex", "red",   readColorCb,  &vertex_color,  0 );
+        ply_set_read_cb( ply, "vertex", "green", readColorCb,  &vertex_color,  0 );
+        ply_set_read_cb( ply, "vertex", "blue",  readColorCb,  &vertex_color,  1 );
+    }
+    if ( vertex_confidence )
+    {
+        ply_set_read_cb( ply, "vertex", "confidence", readVertexCb, &vertex_confidence, 1 );
+    }
+    if ( vertex_intensity )
+    {
+        ply_set_read_cb( ply, "vertex", "intensity", readVertexCb, &vertex_intensity, 1 );
+    }
+    if ( vertex_normal )
+    {
+        ply_set_read_cb( ply, "vertex", "nx", readVertexCb, &vertex_normal, 0 );
+        ply_set_read_cb( ply, "vertex", "ny", readVertexCb, &vertex_normal, 0 );
+        ply_set_read_cb( ply, "vertex", "nz", readVertexCb, &vertex_normal, 1 );
+    }
+
+    if ( face )
+    {
+        ply_set_read_cb( ply, "face", "vertex_indices", readFaceCb, &face, 0 );
+        ply_set_read_cb( ply, "face", "vertex_index", readFaceCb, &face, 0 );
+    }
+
+    if ( point )
+    {
+        ply_set_read_cb( ply, "point", "x", readVertexCb, &point, 0 );
+        ply_set_read_cb( ply, "point", "y", readVertexCb, &point, 0 );
+        ply_set_read_cb( ply, "point", "z", readVertexCb, &point, 1 );
+    }
+    if ( point_color )
+    {
+        ply_set_read_cb( ply, "point", "red",   readColorCb,  &point_color,  0 );
+        ply_set_read_cb( ply, "point", "green", readColorCb,  &point_color,  0 );
+        ply_set_read_cb( ply, "point", "blue",  readColorCb,  &point_color,  1 );
+    }
+    if ( point_confidence )
+    {
+        ply_set_read_cb( ply, "point", "confidence", readVertexCb, &point_confidence, 1 );
+    }
+    if ( point_intensity )
+    {
+        ply_set_read_cb( ply, "point", "intensity", readVertexCb, &point_intensity, 1 );
+    }
+    if ( point_normal )
+    {
+        ply_set_read_cb( ply, "point", "nx", readVertexCb, &point_normal, 0 );
+        ply_set_read_cb( ply, "point", "ny", readVertexCb, &point_normal, 0 );
+        ply_set_read_cb( ply, "point", "nz", readVertexCb, &point_normal, 1 );
+    }
+
+    /* Read ply file. */
+    if ( !ply_read( ply ) )
+    {
+        std::cerr << "Could not read »" << filename << "«."
+            << std::endl;
+    }
+
+
+
+    ply_close( ply );
+    
+    
+    if(vertices)
+    {
+		if(number_of_data > 0)
+		{
+			num_points = number_of_data;
+			
+			V.width = num_points;
+			V.elements = (float*)malloc(number_of_data * 3 * sizeof(float) );
+			
+			for(int i=0; i<number_of_data; i++){
+				int index = (int)((float)rand() * ((float)numVertices-1.0)/RAND_MAX   ) ;
+				
+				float x = vertices[index*3+0] * scale;
+				float y = vertices[index*3+1] * scale;
+				float z = vertices[index*3+2] * scale;
+				
+				//~ printf("%f %f %f \n", x,y,z);
+				
+				V.elements[i * 3] = x;
+				V.elements[i * 3 + 1] = y;
+				V.elements[i * 3 + 2] = z;
+				
+				
+			}
+		}else{
+			
+			V.width = numVertices;
+			V.elements = (float*)malloc(numVertices*3*sizeof(float) );
+			num_points = numVertices;
+			for(int i=0; i<numVertices; i++){
+			
+				V.elements[i*3] = vertices[i*3+0]*scale;
+				V.elements[i*3+1] = vertices[i*3+1]*scale;
+				V.elements[i*3+2] = vertices[i*3+2]*scale;
+			}
+		}
+    }else if(points) {
+		
+		if(number_of_data > 0)
+		{
+			num_points = number_of_data;
+			V.elements = (float*)malloc(number_of_data * 3 * sizeof(float));
+			
+			for(int i=0; i<number_of_data; i++)
+			{
+				int index = (int)((float)rand() * ((float)numPoints-1.0) / RAND_MAX   ) ;
+				
+				float x = points[index*3+0] * scale;
+				float y = points[index*3+1] * scale;
+				float z = points[index*3+2] * scale;
+				
+				//~ printf("%f %f %f \n", x,y,z);
+				
+				V.elements[i*3] = x;
+				V.elements[i*3+1] = y;
+				V.elements[i*3+2] = z;
+				
+			}
+		}else{
+			num_points = numPoints;
+			V.elements = (float*)malloc(numPoints*3*sizeof(float) );
+			
+			for(int i=0; i<numPoints; i++)
+			{
+			
+				V.elements[i*3] = points[i*3+0]*scale;
+				V.elements[i*3+1] = points[i*3+1]*scale;
+				V.elements[i*3+2] = points[i*3+2]*scale;
+			}
+		}
 	}
+    
+    
+    printf("finished reading file: %d\n",(int)numVertices);
+    
+}
 
-        int seed = 12345;
-	srand(seed);
+void writePlyFile(float* V, size_t m_numVertices, float* Result_Normals, size_t m_numVertexNormals, const char* filename, float scale=1.0){
+	
+	/* Handle options. */
+    e_ply_storage_mode mode( PLY_LITTLE_ENDIAN );
+    
+    p_ply oply = ply_create( filename, mode, NULL, 0, NULL );
+    if ( !oply )
+    {
+        std::cerr  << "Could not create »" << filename << "«" << std::endl;
+        return;
+    }
+    
+    
+    float* m_vertices = V;
+    float* m_vertexNormals = Result_Normals;
+    
+    
+    bool vertex_normal     = false;
+    
+    /* Add vertex element. */
+    if ( m_vertices )
+    {
+        ply_add_element( oply, "vertex", m_numVertices );
 
-        int k = 50;
-        int numPoints = 100000;
-        kNearestNeighborSearch(k, numPoints, file, dest);
+        /* Add vertex properties: x, y, z, (r, g, b) */
+        ply_add_scalar_property( oply, "x", PLY_FLOAT );
+        ply_add_scalar_property( oply, "y", PLY_FLOAT );
+        ply_add_scalar_property( oply, "z", PLY_FLOAT );
+        
+        /* Add normals if there are any. */
+        if ( m_vertexNormals )
+        {
+			
+            if ( m_numVertexNormals != m_numVertices )
+            {
+                std::cout << "Amount of vertices and normals"
+                    << " does not match. Normals won't be written." << std::endl;
+            }
+            else
+            {
+                ply_add_scalar_property( oply, "nx", PLY_FLOAT );
+                ply_add_scalar_property( oply, "ny", PLY_FLOAT );
+                ply_add_scalar_property( oply, "nz", PLY_FLOAT );
+                vertex_normal = true;
+            }
+        }
+    }
+    
+    /* Write header to file. */
+    if ( !ply_write_header( oply ) )
+    {
+        std::cerr  << "Could not write header." << std::endl;
+        return;
+    }
+    
+    int no_normal_index = 0;
+    
+    for (size_t i = 0; i < m_numVertices; i++ )
+    {
+		
+		
+        ply_write( oply, (double) m_vertices[ i * 3     ] ); /* x */
+        ply_write( oply, (double) m_vertices[ i * 3 + 1 ] ); /* y */
+        ply_write( oply, (double) m_vertices[ i * 3 + 2 ] ); /* z */
+        
+        if ( vertex_normal )
+        {
+			if(m_vertexNormals[i*3] == 0.0 && m_vertexNormals[i*3+1] == 0.0 && m_vertexNormals[i*3+2] == 0.0){
+				no_normal_index = i;
+			}
+			
+            ply_write( oply, (double) m_vertexNormals[ i * 3     ] ); /* nx */
+            ply_write( oply, (double) m_vertexNormals[ i * 3 + 1 ] ); /* ny */
+            ply_write( oply, (double) m_vertexNormals[ i * 3 + 2 ] ); /* nz */
+        }
+    }
+    
+    //~ std::cout << "last No normal index: " << no_normal_index << std::endl;
+    
+    if ( !ply_close( oply ) )
+    {
+       std::cerr  << "Could not close file." << std::endl;
+    }
+    
+    printf("finished writing points: %d\n", (int)m_numVertices);
+}
 
-        cout << endl << "Terminated" << endl;
+int main(int argc, char** argv){
+	
+	const char* in_file = "/home/amock/datasets/polizei/raw/polizei30M_cut.ply";
+	const char* out_file = "polizei30M_cut_normals.ply";
+	
+	size_t point_size = 5000;
+	int point_dim = 3;
+	
+	PointArray points;
+	PointArray normals;
+	normals.dim = 3;
+	
+	
+	//~ points.elements = (float*)malloc(point_size * 3 * sizeof(float));
+	//~ for(int i=0; i< point_size*3; i++){
+		//~ points.elements[i]=i;
+	//~ }
+	readPlyFile(points, point_size, in_file, 1.0);
+	points.width = point_size;
+	points.dim = 3;
+	
+	//~ printf("main first point: %f %f %f\n", points.elements[0], points.elements[1], points.elements[2]);
+	//~ printf("%d\n", point_size);
+	
+	
+	
+	CalcNormalsCuda calculator(points);
+	
+	calculator.setK(50);
+	calculator.setFlippoint(100000.0, 100000.0, 100000.0);
+	calculator.setMethod("PCA");
+	
+	calculator.start();
+	
+	calculator.getNormals(normals);
+	
+	//~ printf("first normal %f %f %f \n", normals.elements[0], normals.elements[1], normals.elements[2]);
+	
+	writePlyFile(points.elements, point_size, normals.elements, point_size, out_file);
+	
+	free(points.elements);
+	return 1;
 }
